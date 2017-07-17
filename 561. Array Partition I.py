0b348651fbc76de1e0bd61d7c82ec34957dbfae9
LlyyJ <lemon.hust@gmail.com>
2017-07-17 13:40:43 +0800

    Update 561. Array Partition I.py

--- 561. Array Partition I.py
+++ 561. Array Partition I.py
@@ -1,5 +1,6 @@
 """
-Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) 
+which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
 
 Example 1:
 Input: [1,4,3,2]
@@ -25,3 +26,5 @@ class Solution(object):
             if not idx%2:
                 sum += value
         return sum
+
+#better way: use array slice instead of loops
