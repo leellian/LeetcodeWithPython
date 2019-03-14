89945b67f984fec233d4ab13e4230a6fc3c4bfb7
余毓杰 <yuyujie@corp.netease.com>
2019-03-14 13:35:21 +0800

    array # [34] Find First and Last Position of Element in Sorted Array

--- Array/34.find-first-and-last-position-of-element-in-sorted-array.py
+++ Array/34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -38,34 +38,62 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        #1. O(N)
+        # N = len(nums)
+        # l,r = 0,N-1
+        # l_pos,r_pos = -1,-1
+        # while l<=r:
+        #     mid = (l+r)//2
+        #     if nums[mid]==target:
+        #         l_pos = r_pos = mid
+        #         mv_l = mv_r = mid
+        #         #print 'midddddd',mid,l,r
+        #         while 0<=l<=mv_l:
+        #             #print 'mv_llllll',mv_l
+        #             if nums[mv_l]==target:
+        #                 l_pos = mv_l
+        #                 mv_l -= 1
+        #             else:
+        #                 break
+        #         while mv_r<=r<=N-1:
+        #             #print 'mv_rrrrrr',mv_r
+        #             if nums[mv_r]==target:
+        #                 r_pos = mv_r
+        #                 mv_r += 1
+        #             else:
+        #                 break
+        #         return [l_pos,r_pos]
+        #     elif nums[mid]<target<=nums[r]:
+        #         l = mid+1
+        #     else:
+        #         r = mid-1
+        # return [l_pos,r_pos]
+
+        #2. log(N)
         N = len(nums)
-        l,r = 0,N-1
-        l_pos,r_pos = -1,-1
-        while l<=r:
-            mid = (l+r)//2
+        lo,hi = 0,N-1
+        left,right = N,-1
+        while lo<=hi:
+            mid = (lo+hi)//2
             if nums[mid]==target:
-                l_pos = r_pos = mid
-                mv_l = mv_r = mid
-                #print 'midddddd',mid,l,r
-                while 0<=l<=mv_l:
-                    #print 'mv_llllll',mv_l
-                    if nums[mv_l]==target:
-                        l_pos = mv_l
-                        mv_l -= 1
-                    else:
-                        break
-                while mv_r<=r<=N-1:
-                    #print 'mv_rrrrrr',mv_r
-                    if nums[mv_r]==target:
-                        r_pos = mv_r
-                        mv_r += 1
-                    else:
-                        break
-                return [l_pos,r_pos]
-            elif nums[mid]<target<=nums[r]:
-                l = mid+1
+                left = min(left,mid)
+                hi = mid-1
+            elif nums[mid]>target:
+                hi = mid-1
             else:
-                r = mid-1
-        return [l_pos,r_pos]
+                lo = mid+1
+        
+        lo,hi = 0,N-1
+        while lo<=hi:
+            mid = (lo+hi)//2
+            if nums[mid]==target:
+                right = max(right,mid)
+                lo = mid+1
+            elif nums[mid]>target:
+                hi = mid-1
+            else:
+                lo = mid+1
 
-
+        left = left if left!=N else -1
+        right = right if right!=-1 else -1
+        return [left,right]
