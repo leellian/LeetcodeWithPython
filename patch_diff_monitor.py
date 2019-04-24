d51aef1099d2102cbf7207575a6135f2a864a533
余毓杰 <yuyujie@corp.netease.com>
2019-04-24 18:55:32 +0800

    1

--- patch_diff_monitor.py
+++ patch_diff_monitor.py
@@ -2,19 +2,23 @@
 
 import os
 import hashlib
+import sys
+from sendpopo import SendMsg
 
 FLAG = ['NEW','MOD','SAME']
+MD5_FILE = 'MD5.json'
+CHECK_PATH = 'C:\\Users\\yuyujie\Desktop\\facebook\\2019.04.19'
 
 class MD5Check(object):
 	def __init__(self,path):
 		self._working_directory = path
 		self._MD5_dict = {}
 		self._compare_MD5_result = {}
-		self.output = {f:[] for f in FLAG}
-		self.files_path = []
+		self._output = {f:[] for f in FLAG}
+		self._files_path = []
 		
-	def MD5Generate(self):
-		for file in self.files_path:
+	def _MD5Generate(self):
+		for file in self._files_path:
 			with open(file,'r') as f:
 				md5 = hashlib.md5()
 				while True:
@@ -24,8 +28,8 @@ class MD5Check(object):
 					md5.update(t)
 				self._MD5_dict.setdefault(file,md5.hexdigest())
 	
-	def MD5Compare(self):
-		for file in self.files_path:
+	def _MD5Compare(self):
+		for file in self._files_path:
 			self._compare_MD5_result.setdefault(file,FLAG[0])
 			if file in self._MD5_dict.keys():
 				file_md5 = ''
@@ -40,24 +44,45 @@ class MD5Check(object):
 					file_md5 = md5.hexdigest()
 				self._compare_MD5_result[file] = FLAG[2] if file_md5==original_md5 else FLAG[1]
 	
-	def MD5CompareResultOutput(self):
-		for key,val in self._compare_MD5_result.iteritems():
-			self.output[val].append(key)
-	
-	def GetFiles(self):
+	def _GetFiles(self):
 		for root,_,files in os.walk(self._working_directory):
 			for file in files:
 				#TODO filter files not needed
-				self.files_path.append(os.path.join(root,file))
+				self._files_path.append(os.path.join(root,file))
+	
+	def _SaveMD5ToFile(self):
+		with open(MD5_FILE,"w") as f:
+			json.dump(self._MD5_dict,f)
+	
+	def _LoadMD5FromFile(self):
+		with open(MD5_FILE,"r") as f:
+			self._MD5_dict = json.load(f)
+	
+	def UpdateMD5File(self):
+		self._GetFiles()
+		self._MD5Generate()
+		self._SaveMD5ToFile()
+	
+	def MD5CompareResultOutput(self):
+		self._GetFiles()
+		self._LoadMD5FromFile()
+		self._MD5Compare()
+		for key,val in self._compare_MD5_result.iteritems():
+			self._output[val].append(key)
+		return self._output
 	
 def main():
-	path = 'C:\\Users\\yuyujie\Desktop\\facebook\\2019.04.19'
-	checker = MD5Check(path)
-	checker.GetFiles()
-	checker.MD5Generate()
-	checker.MD5Compare()
-	checker.MD5CompareResultOutput()
-	print 1
+	checker = MD5Check(CHECK_PATH)
+	check_result = []
+	if len(sys.argv) == 2: # need to update md5 file
+		checker.UpdateMD5File()
+		SendMsg('yuyujie@corp.netease.com',"已更新所有文件的MD5值","#color#120-10-0")
+		return
+	else:
+		check_result = checker.MD5CompareResultOutput()
+	# ['NEW','MOD','SAME']
+	
+	SendMsg('yuyujie@corp.netease.com',diff_result,"#color#120-10-0")
 	
 if __name__ == "__main__":
 	main()
